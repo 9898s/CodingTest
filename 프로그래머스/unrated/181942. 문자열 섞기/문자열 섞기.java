0837348db8daafae6cc95ac5c6bc92edd02c9cba
CJ <sueh9898@icloud.com>
2023-04-30 03:57:55 +0900

    [unrated] Title: 문자열 섞기, Time: 0.03 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -1,11 +1,13 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
         
         for(int i = 0; i < str1.length(); i++) {
-            answer += str1.charAt(i); 
-            answer += str2.charAt(i);
+            sb.append(str1.charAt(i)).append(str2.charAt(i));
         }
+        
+        answer = sb.toString();
         return answer;
     }
 }
