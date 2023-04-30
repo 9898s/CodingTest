e25ffabcca4ffc600870204f06659773233156af
CJ <sueh9898@icloud.com>
2023-04-30 16:36:22 +0900

    [unrated] Title: 순서 바꾸기, Time: 2.64 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = n; i < num_list.length; i++) {
+            numbers.add(num_list[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            numbers.add(num_list[i]);
+        }
+        
+        int[] answer = numbers.stream().mapToInt(i -> i).toArray();
+        return answer;
+    }
+}
