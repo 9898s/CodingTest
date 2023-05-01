c6900e452e0a4d1373902c3d6f68e0e18e12aa1f
CJ <sueh9898@icloud.com>
2023-05-02 01:31:57 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 1.78 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[arr.length];
         for(int i = 0; i < queries.length; i++) {
             for(int j = queries[i][0]; j <= queries[i][1]; j++) {
                 if(j % queries[i][2] == 0) {
@@ -8,10 +7,6 @@ class Solution {
                 }
             }
         }
-        
-        for(int i = 0; i < arr.length; i++) {
-            answer[i] = arr[i];
-        }
-        return answer;
+        return arr;
     }
 }
