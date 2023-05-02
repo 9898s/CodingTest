e5e326207ba993705f0ef1d24a349f8d06765676
CJ <sueh9898@icloud.com>
2023-05-02 20:42:46 +0900

    [unrated] Title: 리스트 자르기, Time: 2.08 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        switch (n) {
+            case 1:
+                for (int i = 0; i <= slicer[1]; i++) {
+                    arr.add(num_list[i]);
+                }
+                break;
+            case 2:
+                for (int i = slicer[0]; i < num_list.length; i++) {
+                    arr.add(num_list[i]);
+                }
+                break;
+            case 3:
+                for (int i = slicer[0]; i <= slicer[1]; i++) {
+                    arr.add(num_list[i]);
+                }
+                break;
+            case 4:
+                for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
+                    arr.add(num_list[i]);
+                }
+                break;
+        }
+        
+        int[] answer = arr.stream().mapToInt(i -> i).toArray();
+        return answer;
+    }
+}
