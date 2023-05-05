bf0feba401d3bebf6bd9ea2b9e83935924168b33
CJ <sueh9898@icloud.com>
2023-05-05 23:59:56 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.07 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String phone_number) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < phone_number.length() - 4; i++) {
+            sb.append("*");
+        }
+        sb.append(phone_number.substring(phone_number.length() - 4));
+        return sb.toString();
+    }
+}
