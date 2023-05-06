26d26350de88697e94804c829aea6d09bb4c291a
CJ <sueh9898@icloud.com>
2023-05-06 12:42:43 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.09 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int n) {
+        StringBuffer sb = new StringBuffer(Integer.toString(n, 3)).reverse();
+        int answer = Integer.parseInt(sb.toString(), 3);
+        return answer;
+    }
+}
