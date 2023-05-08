b893a541a13a89113d589b48b7616a8ac91eebe7
CJ <sueh9898@icloud.com>
2023-05-08 23:52:42 +0900

    [level 1] Title: 소수 찾기, Time: 21.44 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,34 +1,23 @@
 class Solution {
-    public static int test(int n) {
+    public int solution(int n) {
+        int answer = 0;
         boolean[] isPrime = new boolean[n + 1];
-        int primeCount = 0;
 
-        // 초기화: 모든 수를 소수로 가정
-        for (int i = 2; i <= n; i++) {
+        for (int i = 0; i < n + 1; i++) {
             isPrime[i] = true;
         }
 
-        // 에라토스테네스의 체 알고리즘
         for (int i = 2; i * i <= n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= n; j += i) {
-                    isPrime[j] = false;
-                }
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
             }
         }
 
-        // 소수 개수 세기
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i < n + 1; i++) {
             if (isPrime[i]) {
-                primeCount++;
+                answer++;
             }
         }
-
-        return primeCount;
-    }
-    
-    public int solution(int n) {
-        int answer = test(n);
         return answer;
     }
 }
