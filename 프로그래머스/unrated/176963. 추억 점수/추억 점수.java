c2d27e20e5e8d80bf1af01e129ef97b70737e367
CJ <sueh9898@icloud.com>
2023-05-10 18:35:02 +0900

    [unrated] Title: 추억 점수, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -3,20 +3,18 @@ import java.util.HashMap;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-        HashMap<String, Integer> map = new HashMap<>();
-        
+        HashMap<String, Integer> hm = new HashMap<>();
+
         for (int i = 0; i < name.length; i++) {
-            map.put(name[i], yearning[i]);
+            hm.put(name[i], yearning[i]);
         }
 
         for (int i = 0; i < photo.length; i++) {
-            int sum = 0;
             for (int j = 0; j < photo[i].length; j++) {
-                if (map.containsKey(photo[i][j])) {
-                    sum += map.get(photo[i][j]);
+                if (hm.containsKey(photo[i][j])) {
+                    answer[i] += hm.get(photo[i][j]);
                 }
             }
-            answer[i] = sum;
         }
         return answer;
     }
