9bd65b8e13e1665523eb4c9e44de561c81fee984
CJ <sueh9898@icloud.com>
2023-05-11 12:12:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.48 ms, Memory: 95 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        for (String str : participant) {
+            hm.put(str, hm.getOrDefault(str, 0) + 1);
+        }
+
+        for (String str : completion) {
+            if (hm.containsKey(str)) {
+                hm.put(str, hm.get(str) - 1);
+            }
+        }
+
+        String answer = "";
+        for (String str : hm.keySet()) {
+            if (hm.get(str) > 0) {
+                answer = str;
+                break;
+            }
+        }
+        return answer;
+    }
+}
