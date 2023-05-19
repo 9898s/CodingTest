c8bc18b391033adee7e0c0ecb06ff718ce5c8576
CJ <sueh9898@icloud.com>
2023-05-19 18:07:18 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.55 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,13 +1,17 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int a, int b, int c) {
+        int[] arr = {a, b, c};
+        Arrays.sort(arr);
+
         int answer = 0;
-        
-        if(a != b && a != c && b != c) {
+        if (arr[0] != arr[1] && arr[1] != arr[2]) {
             answer = a + b + c;
-        } else if(a == b && a != c || a == c && a != b || b == c && b != a) {
-            answer = (a + b + c) * (a * a + b * b + c * c);
-        } else if(a == b && b == c) {
-            answer = (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+        } else if (arr[0] == arr[1] && arr[1] != arr[2] || arr[1] == arr[2] && arr[0] != arr[1]) {
+            answer = (a + b + c) * (int) (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+        } else if (arr[0] == arr[2]) {
+            answer = (a + b + c) * (int) (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (int) (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
         }
         return answer;
     }
