e2d2a975999d587a3798ed2db17f12de2a33c2de
CJ <sueh9898@icloud.com>
2023-05-19 18:13:44 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.12 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,19 +1,17 @@
 class Solution {
     public int solution(int[] num_list) {
-        int answer = 0;
+        StringBuffer odd = new StringBuffer();
+        StringBuffer even = new StringBuffer();
         
-        StringBuilder a = new StringBuilder();
-        StringBuilder b = new StringBuilder();
-
-        for(int i : num_list) {
-            if(i % 2 == 1) {
-                a.append(i);
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 1) {
+                odd.append(num_list[i]);
             } else {
-                b.append(i);
+                even.append(num_list[i]);
             }
         }
         
-        answer = Integer.parseInt(a.toString()) + Integer.parseInt(b.toString());
+        int answer = Integer.parseInt(odd.toString()) + Integer.parseInt(even.toString());
         return answer;
     }
 }
