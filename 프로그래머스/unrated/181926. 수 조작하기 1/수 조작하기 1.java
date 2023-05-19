951778232e01eba17469777f8cf03426cb7696eb
CJ <sueh9898@icloud.com>
2023-05-19 18:19:10 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.74 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        
+        for (char ch : control.toCharArray()) {
+            if(ch == 'w') {
+                answer += 1;
+            } else if(ch == 's') {
+                answer -= 1;
+            } else if(ch == 'd') {
+                answer += 10;
+            } else if(ch == 'a') {
+                answer -= 10;
+            }
+        }
+        return answer;
+    }
+}
