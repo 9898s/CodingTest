f37d8b49eb15506c53a5e68cf71605148ad51719
CJ <sueh9898@icloud.com>
2023-05-19 18:22:35 +0900

    [unrated] Title: 수 조작하기 2, Time: 11.56 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,18 +1,17 @@
 class Solution {
     public String solution(int[] numLog) {
-        StringBuilder answer = new StringBuilder();
-        
-        for(int i = 0; i < numLog.length - 1; i++) {
-            if(numLog[i] - numLog[i + 1] == 1) {
-                answer.append("s");
-            } else if(numLog[i] - numLog[i + 1] == -1) {
-                answer.append("w");
-            } else if(numLog[i] - numLog[i + 1] == 10) {
-                answer.append("a");
-            } else if(numLog[i] - numLog[i + 1] == -10) {
-                answer.append("d");
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < numLog.length - 1; i++) {
+            if (numLog[i + 1] - numLog[i] == 1) {
+                sb.append("w");
+            } else if (numLog[i + 1] - numLog[i] == -1) {
+                sb.append("s");
+            } else if (numLog[i + 1] - numLog[i] == 10) {
+                sb.append("d");
+            } else if (numLog[i + 1] - numLog[i] == -10) {
+                sb.append("a");
             }
         }
-        return answer.toString();
+        return sb.toString();
     }
 }
