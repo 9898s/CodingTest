58458ddcac00a9c70418abfe0e34acd2dcb80008
CJ <sueh9898@icloud.com>
2023-05-19 18:17:12 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,18 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length + 1];
-        
-        for(int i = 0; i < answer.length; i++) {
-            if(i == answer.length - 1) {
-                if(num_list[answer.length - 2] > num_list[answer.length - 3]) {
-                    answer[i] = num_list[answer.length - 2] - num_list[answer.length - 3];
-                } else {
-                    answer[i] = num_list[answer.length - 2] * 2;
-                }
-            } else {
-                answer[i] = num_list[i];
-            }
-        }
+        int[] answer = Arrays.copyOf(num_list, num_list.length + 1);
+        answer[answer.length - 1] = answer[answer.length - 2] > answer[answer.length - 3] ? answer[answer.length - 2] - answer[answer.length - 3] : answer[answer.length - 2] * 2;
         return answer;
     }
 }
