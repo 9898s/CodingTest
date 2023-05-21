33aa0bbaa1ea56c7341d64ee152585b8097fb034
CJ <sueh9898@icloud.com>
2023-05-21 14:44:42 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.37 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
@@ -3,24 +3,38 @@ import java.util.Arrays;
 class Solution {
     public int solution(int a, int b, int c, int d) {
         int answer = 0;
+        
         int[] dice = {a, b, c, d};
         Arrays.sort(dice);
-
-        if (dice[0] == dice[3]) {
+        
+        // 모두 같음
+        if(dice[0] == dice[3]) {
             answer = 1111 * dice[0];
-        } else if (dice[0] == dice[2]) {
-            answer = (int) Math.pow(10 * dice[0] + dice[3], 2);
-        } else if (dice[1] == dice[3]) {
-            answer = (int) Math.pow(10 * dice[1] + dice[0], 2);
-        } else if (dice[0] == dice[1] && dice[2] == dice[3] && dice[1] != dice[2]) {
-            answer = (dice[1] + dice[2]) * Math.abs(dice[1] - dice[2]);
-        } else if (dice[0] == dice[1] && dice[2] != dice[3] && dice[1] != dice[2]) {
+        }
+        
+        // 세 주사위
+        else if(dice[0] == dice[2] && dice[2] != dice[3]) {
+            answer = (int)Math.pow(10 * dice[0] + dice[3], 2);
+        } else if(dice[1] == dice[3] && dice[0] != dice[1]) {
+            answer = (int)Math.pow(10 * dice[1] + dice[0], 2);
+        } 
+        
+        // 두 주사위 1
+        else if(dice[0] == dice[1] && dice[2] == dice[3] && dice[1] != dice[2]) {
+            answer = (dice[0] + dice[2]) * Math.abs(dice[0] - dice[2]);
+        }
+        
+        // 두 주사위 2
+        else if(dice[0] == dice[1] && dice[1] != dice[2] && dice[2] != dice[3]) {
             answer = dice[2] * dice[3];
-        } else if (dice[1] == dice[2] && dice[2] != dice[3] && dice[0] != dice[1]) {
+        } else if(dice[1] == dice[2] && dice[0] != dice[1] && dice[0] != dice[3]) {
             answer = dice[0] * dice[3];
-        } else if (dice[2] == dice[3] && dice[0] != dice[1] && dice[1] != dice[2]) {
+        } else if(dice[2] == dice[3] && dice[0] != dice[2] && dice[0] != dice[1]) {
             answer = dice[0] * dice[1];
-        } else {
+        } 
+        
+        // 네 주사위
+        else if(dice[0] != dice[1] && dice[1] != dice[2] && dice[2] != dice[3]) {
             answer = dice[0];
         }
         return answer;
