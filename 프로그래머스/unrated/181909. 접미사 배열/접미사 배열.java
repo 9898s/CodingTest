e42506787f4208378d73d6eb301b404623818381
CJ <sueh9898@icloud.com>
2023-05-23 12:07:55 +0900

    [unrated] Title: 접미사 배열, Time: 0.26 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -2,11 +2,13 @@ import java.util.Arrays;
 
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = new String[my_string.length()];
-        for(int i = 0; i < my_string.length(); i++) {
-            answer[i] = my_string.substring(i);
+        String[] answer = new String[my_string.toCharArray().length];
+        
+        answer[0] = my_string;
+        for (int i = 1; i < my_string.length(); i++) {
+            answer[i] = my_string.substring(my_string.length() - i);
         }
-
+        
         Arrays.sort(answer);
         return answer;
     }
