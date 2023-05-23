c26a14bccc32e7d6320b3d4252186164ebb7ba98
CJ <sueh9898@icloud.com>
2023-05-23 12:24:19 +0900

    [unrated] Title: 세로 읽기, Time: 12.89 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -1,10 +1,10 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
-        StringBuilder answer = new StringBuilder();
+        String answer = "";
         
-        for(int i = c - 1; i < my_string.length(); i += m) {
-            answer.append(my_string.charAt(i));
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            answer += my_string.charAt(i);
         }
-        return answer.toString();
+        return answer;
     }
 }
