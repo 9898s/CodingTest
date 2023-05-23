9ba42a82c2118050aeeae4be79b45a3620e01473
CJ <sueh9898@icloud.com>
2023-05-23 12:13:47 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -2,17 +2,7 @@ import java.util.HashMap;
 
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        int answer = 0;
-        
-        HashMap<String, String> hm = new HashMap();
-        for (int i = 0; i < my_string.length(); i++) {
-            String sub = my_string.substring(i, my_string.length());
-            hm.put(sub, sub);
-        }
-        
-        if (hm.containsKey(is_suffix)) {
-            answer = 1;
-        }
+        int answer = my_string.endsWith(is_suffix) ? 1 : 0;
         return answer;
     }
 }
