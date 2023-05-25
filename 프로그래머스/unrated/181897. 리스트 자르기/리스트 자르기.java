4d9682a07640fbc78da083cd74e78be15608827f
CJ <sueh9898@icloud.com>
2023-05-25 22:10:31 +0900

    [unrated] Title: 리스트 자르기, Time: 2.11 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -2,31 +2,25 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        switch (n) {
-            case 1:
-                for (int i = 0; i <= slicer[1]; i++) {
-                    arr.add(num_list[i]);
-                }
-                break;
-            case 2:
-                for (int i = slicer[0]; i < num_list.length; i++) {
-                    arr.add(num_list[i]);
-                }
-                break;
-            case 3:
-                for (int i = slicer[0]; i <= slicer[1]; i++) {
-                    arr.add(num_list[i]);
-                }
-                break;
-            case 4:
-                for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
-                    arr.add(num_list[i]);
-                }
-                break;
-        }
+        ArrayList<Integer> list = new ArrayList<>();
         
-        int[] answer = arr.stream().mapToInt(i -> i).toArray();
-        return answer;
+        if (n == 1) {
+            for (int i = 0; i <= slicer[1]; i++) {
+                list.add(num_list[i]);
+            }
+        } else if (n == 2) {
+            for (int i = slicer[0]; i < num_list.length; i++) {
+                list.add(num_list[i]);
+            }
+        } else if (n == 3) {
+            for (int i = slicer[0]; i <= slicer[1]; i++) {
+                list.add(num_list[i]);
+            }
+        } else if (n == 4) {
+            for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
+                list.add(num_list[i]);
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
