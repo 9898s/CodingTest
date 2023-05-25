1a0dd048444cf625c809fbfd97f9163270f100d6
CJ <sueh9898@icloud.com>
2023-05-25 23:18:00 +0900

    [unrated] Title: 배열 조각하기, Time: 4.73 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -1,26 +1,28 @@
-import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i : arr) {
-            list.add(i);
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < arr.length; i++) {
+            deque.add(arr[i]);
         }
 
         for (int i = 0; i < query.length; i++) {
-            if(i % 2 == 0) {
-                int size = list.size() - query[i] - 1;
-                for (int j = 0; j < size; j++) {
-                    list.remove(list.size() - 1);
+            int count = 0;
+
+            if (i % 2 == 0) {
+                count = deque.size() - query[i] - 1;
+                for (int j = 0; j < count; j++) {
+                    deque.removeLast();
                 }
             } else {
-                for (int j = 0; j < query[i]; j++) {
-                    list.remove(0);
+                count = query[i];
+                for (int j = 0; j < count; j++) {
+                    deque.removeFirst();
                 }
             }
         }
-
-        int[] answer = list.stream().mapToInt(i -> i).toArray();
-        return answer;
+        return deque.stream().mapToInt(Integer::intValue).toArray();
     }
 }
