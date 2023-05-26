5db9273d0dc2744826867d483bb6ff99e9eec323
CJ <sueh9898@icloud.com>
2023-05-27 05:53:55 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.39 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,30 +1,53 @@
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        HashMap<String, HashSet<String>> hm = new HashMap<>();
-        HashMap<String, Integer> hm2 = new HashMap<>();
-
-        for (int i = 0; i < id_list.length; i++) {
-            hm.put(id_list[i], new HashSet<>());
-            hm2.put(id_list[i], i);
-        }
-
-        for (String s : report) {
-            String[] str = s.split(" ");
-            hm.get(str[1]).add(str[0]);
-        }
-
         int[] answer = new int[id_list.length];
-        for (int i = 0; i < id_list.length; i++) {
-            HashSet<String> hs = hm.get(id_list[i]);
-            if (hs.size() >= k) {
-                for (String str : hs) {
-                    answer[hm2.get(str)]++;
-                }
-            }
+        ArrayList<User> users = new ArrayList<>();
+        HashMap<String,Integer> suspendedList = new HashMap<>(); //<이름>
+        HashMap<String,Integer> idIdx = new HashMap<String,Integer>(); // <이름, 해당 이름의 User 클래스 idx>
+        int idx = 0;
+
+        for(String name : id_list) {
+            idIdx.put(name,idx++);
+            users.add(new User(name));
         }
+
+        for(String re : report){
+            String[] str = re.split(" ");
+            //suspendedCount.put(str[0], suspendedCount.getOrDefault(str[0],0)+1);
+            users.get( idIdx.get(str[0])).reportList.add(str[1]);
+            users.get( idIdx.get(str[1])).reportedList.add(str[0]);
+        }
+
+        for(User user : users){
+            if(user.reportedList.size() >= k)
+                suspendedList.put(user.name,1);
+        }
+
+         for(User user : users){
+             for(String nameReport : user.reportList){
+                 if(suspendedList.get(nameReport) != null){
+                     answer[idIdx.get(user.name)]++;
+                 }
+
+             }
+        }
+
+
+
+
         return answer;
+    }
+}
+
+class User{
+    String name;
+    HashSet<String> reportList;
+    HashSet<String> reportedList;
+    public User(String name){
+        this.name = name;
+        reportList = new HashSet<>();
+        reportedList = new HashSet<>();
     }
 }
