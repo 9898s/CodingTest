655e3ae75059c40426f0140760711a7d810b036c
CJ <sueh9898@icloud.com>
2023-06-02 21:53:52 +0900

    [level 2] Title: 구명보트, Time: 9.49 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -1,26 +1,19 @@
 import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
 
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
 
-        Deque<Integer> deque = new LinkedList<>();
-        for (int i = 0; i < people.length; i++) {
-            deque.add(people[i]);
-        }
+        int i = 0;
+        int j = people.length - 1;
 
-        while (deque.size() > 0) {
-            if (deque.getFirst() + deque.getLast() <= limit) {
-                deque.removeFirst();
-
-                if (!deque.isEmpty()) {
-                    deque.removeLast();
-                }
+        while (i <= j) {
+            if (people[i] + people[j] > limit) {
+                j--;
             } else {
-                deque.removeLast();
+                i++;
+                j--;
             }
             answer++;
         }
