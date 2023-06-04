eacc75afcc76f8f5a94d7f3ad69daafe4a03bce7
CJ <sueh9898@icloud.com>
2023-06-05 04:17:04 +0900

    [level 1] Title: 시저 암호, Time: 0.56 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,24 +1,27 @@
 class Solution {
     public String solution(String s, int n) {
-        StringBuffer sb = new StringBuffer();
-
-        for (char item : s.toCharArray()) {
-            if (item >= 'A' && item <= 'Z') {
-                if (item + n <= 'Z') {
-                    sb.append((char) (item + n));
-                } else {
-                    sb.append((char) (item + n - 26));
+        String answer = "";
+        char[] chars = s.toCharArray();
+        
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == ' ') {
+                continue;
+            }
+            
+            if (chars[i] >= 'a' && chars[i] <= 'z') {
+                chars[i] += n;
+                if (chars[i] > 'z') {
+                    chars[i] = (char) ('a' + (chars[i] - 'z') - 1);
                 }
-            } else if (item >= 'a' && item <= 'z') {
-                if (item + n <= 'z') {
-                    sb.append((char) (item + n));
-                } else {
-                    sb.append((char) (item + n - 26));
+            } else if (chars[i] >= 'A' && chars[i] <= 'Z') {
+                chars[i] += n;
+                if (chars[i] > 'Z') {
+                    chars[i] = (char) ('A' + (chars[i] - 'Z') - 1);
                 }
-            } else {
-                sb.append(item);
             }
         }
-        return sb.toString();
+        
+        answer = String.valueOf(chars);
+        return answer;
     }
 }
