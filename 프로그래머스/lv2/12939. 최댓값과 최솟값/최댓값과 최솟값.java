576961832162c4ed971b6760855384b9aab2cae7
CJ <sueh9898@icloud.com>
2023-06-05 02:54:28 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.55 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,15 +2,17 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String s) {
+        String answer = "";
+        
         String[] str = s.split(" ");
-
-        int[] number = new int[str.length];
-        for (int i = 0; i < number.length; i++) {
-            number[i] = Integer.parseInt(str[i]);
+        int[] numbers = new int[str.length];
+        
+        for (int i = 0; i < str.length; i++) {
+            numbers[i] = Integer.parseInt(str[i]);
         }
-
-        Arrays.sort(number);
-        String answer = number[0] + " " + number[number.length - 1];
+        Arrays.sort(numbers);
+        
+        answer = numbers[0] + " " + numbers[numbers.length - 1];
         return answer;
     }
 }
