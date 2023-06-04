21b82f51838dc21e42d8313b738e4d0c794a679c
CJ <sueh9898@icloud.com>
2023-06-05 02:14:00 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.07 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,13 +1,7 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
-        
-        if(n % 2 == 0) {
-            answer = "수박".repeat(n / 2);
-        } else {
-            answer = "수박".repeat(n / 2 + 1);
-            answer = answer.substring(0, answer.length() - 1);
-        }
+        String answer = "수박".repeat((n + 1) / 2);
+        answer = answer.substring(0, n);
         return answer;
     }
 }
