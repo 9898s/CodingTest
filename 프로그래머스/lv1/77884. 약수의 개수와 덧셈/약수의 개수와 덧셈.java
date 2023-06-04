67d7f967d419734115e5cdd9cc4a82a99ad6f505
CJ <sueh9898@icloud.com>
2023-06-05 02:21:32 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.10 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,18 +1,20 @@
 class Solution {
-    public static int getDevision(int num) {
-        int result = 1;
-        for(int i = 1; i <= num / 2; i++) {
-            if(num % i == 0) {
-                result++;
+    public int getDivisor(int n) {
+        int count = 1;
+        
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                count++;
             }
         }
-        return result;
+        return count;
     }
     
     public int solution(int left, int right) {
         int answer = 0;
-        for(int i = left; i <= right; i++) {
-            if(getDevision(i) % 2 == 0) {
+        
+        for (int i = left; i <= right; i++) {
+            if (getDivisor(i) % 2 == 0) {
                 answer += i;
             } else {
                 answer -= i;
