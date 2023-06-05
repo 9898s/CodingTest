2fa52b2960298a0444a3c17cf852857132722bb6
CJ <sueh9898@icloud.com>
2023-06-06 04:10:40 +0900

    [unrated] Title: 덧칠하기, Time: 2.33 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,16 +1,16 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
+        int answer = 0;
         boolean[] paint = new boolean[n];
+        
         for (int item : section) {
             paint[item - 1] = true;
         }
-
-        int answer = 0;
+        
         for (int item : section) {
             int idx = item - 1;
             if (paint[idx]) {
-                int maxIdx = idx + m > n ? n : idx + m;
-
+                int maxIdx = n <= idx + m ? n : idx + m;
                 for (int i = idx; i < maxIdx; i++) {
                     paint[i] = false;
                 }
