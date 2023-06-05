1bab26825b86f893787cc2bb063e7699f88b28ef
CJ <sueh9898@icloud.com>
2023-06-06 04:25:23 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,40 +1,37 @@
-import java.util.HashMap;
-
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int zero = 0;
-        HashMap<Integer, Boolean> hm = new HashMap<>();
-
+        int[] answer = new int[2];
+        
+        int zeroCnt = 0;
         for (int item : lottos) {
             if (item == 0) {
-                zero++;
-                continue;
+                zeroCnt++;
             }
-
-            hm.put(item, true);
         }
-
+        
         int win = 0;
-        for (int item : win_nums) {
-            if (hm.containsKey(item)) {
-                win++;
+        for (int i = 0; i < lottos.length; i++) {
+            for (int j = 0; j < win_nums.length; j++) {
+                if (lottos[i] == win_nums[j]) {
+                    win++;
+                }
             }
         }
-
-        int[] answer = {getRank(zero + win), getRank(win)};
+        answer[0] = ranking(zeroCnt + win);
+        answer[1] = ranking(win);
         return answer;
     }
     
-    public static int getRank(int answer) {
-        if (answer == 6) {
+    public int ranking(int win) {
+        if (win == 6) {
             return 1;
-        } else if (answer == 5) {
+        } else if (win == 5) {
             return 2;
-        } else if (answer == 4) {
+        } else if (win == 4) {
             return 3;
-        } else if (answer == 3) {
+        } else if (win == 3) {
             return 4;
-        } else if (answer == 2) {
+        } else if (win == 2) {
             return 5;
         } else {
             return 6;
