19e389e66d6865040aa2255884b8d2f67acea4c3
CJ <sueh9898@icloud.com>
2023-06-06 03:01:54 +0900

    [unrated] Title: 카드 뭉치, Time: 0.12 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -1,27 +1,30 @@
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        Queue<String> cQ1 = new LinkedList<>();
-        Collections.addAll(cQ1, cards1);
-
-        Queue<String> cQ2 = new LinkedList<>();
-        Collections.addAll(cQ2, cards2);
-
-        int count = 0;
-        for (String str : goal) {
-            if (str.equals(cQ1.peek())) {
-                cQ1.poll();
-                count++;
-            } else if (str.equals(cQ2.peek())) {
-                cQ2.poll();
-                count++;
+        String answer = "Yes";
+        
+        Queue<String> queue1 = new LinkedList<>();
+        for (String item : cards1) {
+            queue1.add(item);
+        }
+        
+        Queue<String> queue2 = new LinkedList<>();
+        for (String item : cards2) {
+            queue2.add(item);
+        }
+        
+        for (int i = 0; i < goal.length; i++) {
+            if (goal[i].equals(queue1.peek())) {
+                queue1.remove();
+            } else if (goal[i].equals(queue2.peek())) {
+                queue2.remove();
+            } else {
+                answer = "No";
+                break;
             }
         }
-
-        String answer = (count == goal.length) ? "Yes" : "No";
         return answer;
     }
 }
