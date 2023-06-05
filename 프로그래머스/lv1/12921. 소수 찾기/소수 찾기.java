136da3f87696362df2b524413c4f9174702396a3
CJ <sueh9898@icloud.com>
2023-06-06 02:50:19 +0900

    [level 1] Title: 소수 찾기, Time: 242.65 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,23 +1,23 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        boolean[] isPrime = new boolean[n + 1];
-
-        for (int i = 0; i < n + 1; i++) {
-            isPrime[i] = true;
-        }
-
-        for (int i = 2; i * i <= n; i++) {
-            for (int j = i * i; j <= n; j += i) {
-                isPrime[j] = false;
-            }
-        }
-
-        for (int i = 2; i < n + 1; i++) {
-            if (isPrime[i]) {
+        
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
                 answer++;
             }
         }
         return answer;
+    }
+    
+    public boolean isPrime(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (i * i == n) {
+                return false;
+            } else if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
