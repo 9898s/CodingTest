07e71fa311e3dd63a2c66d13b49c9684c07ffd48
CJ <sueh9898@icloud.com>
2023-06-06 02:40:28 +0900

    [unrated] Title: 과일 장수, Time: 0.37 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -2,21 +2,12 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int answer = 0;
-        int count = score.length / m;
-
         Arrays.sort(score);
-        for (int i = 0; i < count; i++) {
-            int idx = 0;
-            int[] arr = new int[m];
-            for (int j = score.length - 1 - i * m; j >= score.length - 1 - ((i + 1) * m - 1); j--) {
-                arr[idx++] = score[j];
-            }
-
-            Arrays.sort(arr);
-            answer += arr[0] * m;
+        int answer = 0;
+        
+        for (int i = 0; i < score.length / m; i++) {
+            answer += score[score.length - m * (i + 1)] * m;
         }
-        System.out.println(answer);
         return answer;
     }
 }
