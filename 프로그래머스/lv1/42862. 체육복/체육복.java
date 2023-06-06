c821cd8fbf67b89b92c12c84c1273aa816321f74
CJ <sueh9898@icloud.com>
2023-06-07 02:00:24 +0900

    [level 1] Title: 체육복, Time: 0.14 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -2,31 +2,46 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n;
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+        int answer = 0;
+        int[] count = new int[n];
+        Arrays.fill(count, 1);
         
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] - reserve[j] == 0) {
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                }
-            }
-        }
-
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (Math.abs(lost[i] - reserve[j]) == 1) {
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                }
-            }
-        }
-
         for (int item : lost) {
-            if (item != -1) {
-                answer--;
+            count[item - 1]--;
+        }
+        for (int item : reserve) {
+            count[item - 1]++;
+        }
+        
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == 0) {
+                if (i == 0) {
+                    if (count[i + 1] == 2) {
+                        count[i]++;
+                        count[i + 1]--;
+                    }
+                } else if (i == count.length - 1) {
+                    if (count[i - 1] == 2) {
+                        count[i]++;
+                        count[i - 1]--;
+                    }
+                } else {
+                    if (count[i - 1] == 2) {
+                        count[i]++;
+                        count[i - 1]--;
+                    } else if (count[i + 1] == 2) {
+                        count[i]++;
+                        count[i + 1]--;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(Arrays.toString(count));
+        
+        for (int item : count) {
+            if (item > 0) {
+                answer++;
             }
         }
         return answer;
