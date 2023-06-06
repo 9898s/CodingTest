be4b01c0c79ddb456e4fa4bc239a9f18e6b3b688
CJ <sueh9898@icloud.com>
2023-06-07 01:42:07 +0900

    [unrated] Title: 옹알이 (2), Time: 0.66 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,42 +1,21 @@
 class Solution {
     public int solution(String[] babbling) {
-        String[] str = {"aya", "ye", "woo", "ma"};
-
-        for (int i = 0; i < babbling.length; i++) {
-            for (int j = 0; j < str.length; j++) {
-                if (babbling[i].contains(str[j])) {
-                    babbling[i] = babbling[i].replace(str[j], String.valueOf(j));
-                }
-            }
-        }
-
         int answer = 0;
-        for (String s : babbling) {
-            if (isNumberic(s)) {
-                int same = -1;
-                boolean isSame = false;
-                for (char ch : s.toCharArray()) {
-                    if (ch - '0' != same) {
-                        same = ch - '0';
-                    } else {
-                        isSame = true;
-                    }
-                }
-
-                if (!isSame) {
-                    answer++;
-                }
+        String[] tell = {"aya", "ye", "woo", "ma"};
+        String[] repeatTell = {"ayaaya", "yeye", "woowoo", "mama"};
+        
+        for (String str : babbling) {
+            for (String rStr : repeatTell) {
+                str = str.replace(rStr, "0");
+            }
+            for (String tStr : tell) {
+                str = str.replace(tStr, "1");
+            }
+            
+            if (str.replace("1", "").length() == 0) {
+                answer++;
             }
         }
         return answer;
-    }
-    
-    public static boolean isNumberic(String str) {
-        for (char ch : str.toCharArray()) {
-            if (ch - '0' > 9 || ch - '0' < 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
