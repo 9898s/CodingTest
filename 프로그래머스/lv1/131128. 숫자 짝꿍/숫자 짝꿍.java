800cda1e523be03f1056cfcecf4a7074ff9b6ae6
CJ <sueh9898@icloud.com>
2023-06-07 00:41:56 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.12 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String X, String Y) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (char ch : Y.toCharArray()) {
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
+
+        ArrayList<Character> list = new ArrayList<>();
+        for (char ch : X.toCharArray()) {
+            if (hm.containsKey(ch)) {
+                hm.put(ch, hm.get(ch) - 1);
+                list.add(ch);
+
+                if (hm.get(ch) <= 0) {
+                    hm.remove(ch);
+                }
+            }
+        }
+        
+        if (list.size() == 0) {
+            return "-1";
+        }
+        
+        if (list.size() > 1) {
+            if (list.get(0) == '0' && list.get(1) == '0') {
+                return "0";
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Collections.sort(list, Collections.reverseOrder());
+        for (char ch : list) {
+            sb.append(ch);
+        }
+        
+        String answer = sb.toString();
+        return answer;
+    }
+}
