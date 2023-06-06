f610f8605c007be1a59a613945ac595171484778
CJ <sueh9898@icloud.com>
2023-06-07 00:53:56 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,40 +1,29 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-
 class Solution {
     public String solution(String X, String Y) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-        for (char ch : Y.toCharArray()) {
-            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
-        }
+        int[] X1 = new int[10];
+        int[] Y1 = new int[10];
 
-        ArrayList<Character> list = new ArrayList<>();
         for (char ch : X.toCharArray()) {
-            if (hm.containsKey(ch)) {
-                hm.put(ch, hm.get(ch) - 1);
-                list.add(ch);
+            X1[ch - '0']++;
+        }
 
-                if (hm.get(ch) <= 0) {
-                    hm.remove(ch);
-                }
-            }
-        }
-        
-        if (list.size() == 0) {
-            return "-1";
-        }
-        if (list.get(0) == '0') {
-            return "0";
+        for (char ch : Y.toCharArray()) {
+            Y1[ch - '0']++;
         }
 
         StringBuilder sb = new StringBuilder();
-        Collections.sort(list, Collections.reverseOrder());
-        for (char ch : list) {
-            sb.append(ch);
+        for (int i = 9; i >= 0; i--) {
+            if (X1[i] > 0 && Y1[i] > 0) {
+                sb.append(String.valueOf(i).repeat(Math.min(X1[i], Y1[i])));
+            }
         }
         
-        String answer = sb.toString();
-        return answer;
+        if (sb.length() == 0) {
+            return "-1";
+        }
+        if (sb.charAt(0) == '0') {
+            return "0";
+        }
+        return sb.toString();
     }
 }
