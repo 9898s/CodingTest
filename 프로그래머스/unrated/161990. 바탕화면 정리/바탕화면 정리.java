607aa5e58cee1c966e5a7370a4928c726c60f264
CJ <sueh9898@icloud.com>
2023-06-09 03:25:18 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.11 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,20 +1,22 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
-        int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
-
+        int[] answer = new int[4];
+        
+        answer[0] = Integer.MAX_VALUE;
+        answer[1] = Integer.MAX_VALUE;
+        
+        answer[2] = Integer.MIN_VALUE;
+        answer[3] = Integer.MIN_VALUE;
+        
         for (int i = 0; i < wallpaper.length; i++) {
-            for (int j = 0; j < wallpaper[i].length(); j++) {
-                if (wallpaper[i].charAt(j) == '#') {
-                    minX = Math.min(minX, i);
-                    minY = Math.min(minY, j);
-
-                    maxX = Math.max(maxX, i + 1);
-                    maxY = Math.max(maxY, j + 1);
-                }
+            if (wallpaper[i].contains("#")) {
+                answer[0] = Math.min(i, answer[0]);
+                answer[1] = Math.min(wallpaper[i].indexOf("#"), answer[1]);
+                
+                answer[2] = Math.max(i + 1, answer[2]);
+                answer[3] = Math.max(wallpaper[i].lastIndexOf("#") + 1, answer[3]);
             }
         }
-        int[] answer = {minX, minY, maxX, maxY};
         return answer;
     }
 }
