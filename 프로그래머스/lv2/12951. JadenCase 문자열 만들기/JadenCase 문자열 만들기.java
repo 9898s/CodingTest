710fe43631118a719d024b850631cdbf530f5cbf
CJ <sueh9898@icloud.com>
2023-06-11 03:31:48 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 8.81 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,22 +1,31 @@
 class Solution {
     public String solution(String s) {
-        s = s.toLowerCase();
+        /*
+        1. s 문자열을 소문자로 변환해준다.
+        2. 논리형 변수 isSpace를 선언해준다. (첫 글자는 무조건 대문자이므로 true로 초기화 해준다.)
+        3. 반복문을 돌려서 문자 하나하나씩 확인해준다.
+        4. 공백 부분이 나타나면 isSpace 변수에 true 값을 넣어준다.
+        5. isSpace 변수가 true일 경우 다음 문자를 대문자로 변환 해주고 isSpace는 false로 바꿔준다.
+        */
         
+        String answer = "";
+        
+        s = s.toLowerCase();
         boolean isSpace = true;
-        StringBuilder sb = new StringBuilder();
-
+        
         for (int i = 0; i < s.length(); i++) {
-            if (isSpace) {
-                sb.append(Character.toUpperCase(s.charAt(i)));
-                isSpace = false;
-            } else {
-                sb.append(s.charAt(i));
-            }
-
-            if (s.charAt(i) == ' ') {
+            char ch = s.charAt(i);
+            if (ch == ' ') {
                 isSpace = true;
             }
+            else {
+                if (isSpace) {
+                    isSpace = false;
+                    ch = Character.toUpperCase(ch);
+                }
+            }
+            answer += ch;
         }
-        return sb.toString();
+        return answer;
     }
 }
