37e1a7dee2817b631f59a9190da3805bb5158d54
CJ <sueh9898@icloud.com>
2023-06-11 03:16:56 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 7.68 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,17 +2,23 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String s) {
+        /*     
+        1. 공백을 기준으로 나눠서 문자열 배열에 넣어준다.
+        2. 정수형 배열을 선언해주고 문자열 배열의 문자를 정수로 변환하고 넣어준다.
+        3. 정수형 배열을 오름차순으로 정렬해준다.
+        */
+        
         String answer = "";
         
-        String[] str = s.split(" ");
-        int[] arr = new int[str.length];
+        String[] strings = s.split(" ");
+        int[] numbers = new int[strings.length];
         
-        for (int i = 0; i < str.length; i++) {
-            arr[i] = Integer.parseInt(str[i]);
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(strings[i]);
         }
-        Arrays.sort(arr);
+        Arrays.sort(numbers);
         
-        answer = arr[0] + " " + arr[arr.length - 1];
+        answer = numbers[0] + " " + numbers[numbers.length - 1];
         return answer;
     }
 }
