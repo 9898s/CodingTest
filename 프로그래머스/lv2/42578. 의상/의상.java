e4767a38b47f0fd5f4614be92718549dc8b01930
CJ <sueh9898@icloud.com>
2023-06-11 02:49:20 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.java
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -2,14 +2,15 @@ import java.util.HashMap;
 
 class Solution {
     public int solution(String[][] clothes) {
+        int answer = 1;
+        
         HashMap<String, Integer> hm = new HashMap<>();
         for (int i = 0; i < clothes.length; i++) {
             hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 0) + 1);
         }
         
-        int answer = 1;
         for (String str : hm.keySet()) {
-            answer *= hm.get(str) + 1;
+            answer *= hm.get(str) + 1; 
         }
         return answer - 1;
     }
