c4e9eaf120376d9b6d184ee5965a0b710d4c1088
CJ <sueh9898@icloud.com>
2023-06-10 19:51:57 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,20 +1,22 @@
 class Solution {
-    public int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        } 
-        return gcd(b, a % b);
-    }
-    
-    public int lcm(int a, int b) {
-        return a * b / gcd(a, b);
-    }
-    
     public int solution(int[] arr) {
-        int answer = arr[0];
+        int answer = 0;
+        
+        answer = arr[0];
         for (int i = 1; i < arr.length; i++) {
             answer = lcm(answer, arr[i]);
         }
         return answer;
+    }
+    
+    public int gcd(int i, int j) {
+        if (i % j == 0) {
+            return j;
+        }
+        return gcd(j, i % j);
+    }
+    
+    public int lcm(int i, int j) {
+        return i * j / gcd(i, j);
     }
 }
