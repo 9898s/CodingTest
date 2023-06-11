a8f2074591f920fd261d3fb1859d2787056df92d
CJ <sueh9898@icloud.com>
2023-06-11 15:58:53 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 42.20 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] solution(String s) {
+        /*
+        1. s 문자열에서 0과 1의 개수를 카운트 해준다.
+        2. 0의 개수는 answer[1]에 더해주고, 1의 개수는 2진수 문자열로 변환해준다.
+        3. 반복 한 횟수만큼 answer[0]에 더해주면 끝
+        */
+        
         int[] answer = new int[2];
-        String s2 = "";
-        int count = 0;
-
-        while (!s.equals("1")) {
-            s2 = s.replaceAll("0", "");
-            count = s.length() - s2.length();
-            s = Integer.toString(s2.length(), 2);
-            
+        while(!s.equals("1")) {
             answer[0]++;
-            answer[1] += count;
+            answer[1] += s.length() - s.replaceAll("0", "").length();
+            s = Integer.toString(s.replaceAll("0", "").length(), 2);
         }
         return answer;
     }
