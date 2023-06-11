e4d7670f92129f5ccdb7d3e33b06ed3eff57d2cf
CJ <sueh9898@icloud.com>
2023-06-11 16:16:50 +0900

    [level 2] Title: 피보나치 수, Time: 1.63 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -5,7 +5,7 @@ class Solution {
         int num1 = 0;
         int num2 = 1;
         
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i < n; i++) {
             answer = (num1 + num2) % 1234567;
             num1 = num2;
             num2 = answer;
