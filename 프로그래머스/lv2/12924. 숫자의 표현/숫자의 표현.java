f1e84f70a37f53e2479461c9751be2c26feea39b
CJ <sueh9898@icloud.com>
2023-06-11 16:04:40 +0900

    [level 2] Title: 숫자의 표현, Time: 1.50 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,17 +1,20 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        /*
+        이중 for문을 돌려서 풀면 된다.
+        1. 더 한 값이 n이 될 경우 break
+        2. 더 한 값이 n을 넘게 될 경우 break
+        */
         
+        int answer = 0;
         for (int i = 1; i <= n; i++) {
             int sum = 0;
             for (int j = i; j <= n; j++) {
                 sum += j;
-                
                 if (sum == n) {
                     answer++;
                     break;
-                }
-                else if (sum > n) {
+                } else if(sum > n) {
                     break;
                 }
             }
