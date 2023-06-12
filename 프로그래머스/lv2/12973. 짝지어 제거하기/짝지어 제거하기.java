577810079fd554c637c56a33d85678be8591e550
CJ <sueh9898@icloud.com>
2023-06-12 21:53:06 +0900

    [level 2] Title: 짝지어 제거하기, Time: 56.14 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -4,6 +4,12 @@ class Solution
 {
     public int solution(String s)
     {
+        /*
+        1. 스택에 문자열을 문자를 하나씩 넣어준다.
+        2. 스택의 peek 값이 넣을 문자와 같을 경우 pop 해준다.
+        3. 아닐 경우 해당 문자를 push 해준다.
+        */
+        
         int answer = 0;
         Stack<Character> stack = new Stack<>();
         
@@ -11,11 +17,13 @@ class Solution
             if (!stack.isEmpty() && stack.peek() == ch) {
                 stack.pop();
             } else {
-                stack.add(ch);
+                stack.push(ch);
             }
         }
         
-        answer = stack.size() == 0 ? 1 : 0;
+        if (stack.isEmpty()) {
+            answer = 1;
+        }
         return answer;
     }
 }
