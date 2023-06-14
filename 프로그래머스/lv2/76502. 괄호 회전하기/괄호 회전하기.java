c8b589ca6417cc7fc198f3cbd662984b4ee2fb45
CJ <sueh9898@icloud.com>
2023-06-14 14:56:03 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.43 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,51 +1,52 @@
-import java.util.ArrayList;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.Stack;
+import java.util.LinkedList;
 
 class Solution {
     public int solution(String s) {
+        /*
+        1. 문자열 길이 만큼 Deque를 이용해서 왼쪽으로 회전 시켜준다.
+        2. 회전 시킬때마다 Stack을 이용해서 올바른 괄호 문자열인지 확인해준다.
+        */
+        
         int answer = 0;
+        
         Deque<Character> deque = new LinkedList<>();
         for (char ch : s.toCharArray()) {
             deque.add(ch);
         }
-
+        
         for (int i = 0; i < deque.size(); i++) {
-            ArrayList<Character> list = new ArrayList<>();
-            for (char ch : deque) {
-                list.add(ch);
-            }
-
+            deque.addLast(deque.removeFirst());
+            
             Stack<Character> stack = new Stack<>();
-            for (int j = 0; j < list.size(); j++) {
-                if (list.get(j) == ']') {
-                    if (!stack.isEmpty() && stack.peek() == '[') {
-                        stack.pop();
-                    } else {
-                        stack.push(list.get(j));
-                    }
-                } else if (list.get(j) == '}') {
-                    if (!stack.isEmpty() && stack.peek() == '{') {
-                        stack.pop();
-                    } else {
-                        stack.push(list.get(j));
-                    }
-                } else if (list.get(j) == ')') {
+            for (char ch : deque) {
+                if (ch == ')') {
                     if (!stack.isEmpty() && stack.peek() == '(') {
                         stack.pop();
                     } else {
-                        stack.push(list.get(j));
+                        stack.add(ch);
+                    }
+                } else if (ch == ']') {
+                    if (!stack.isEmpty() && stack.peek() == '[') {
+                        stack.pop();
+                    } else {
+                        stack.add(ch);
+                    }
+                } else if (ch == '}') {
+                    if (!stack.isEmpty() && stack.peek() == '{') {
+                        stack.pop();
+                    } else {
+                        stack.add(ch);
                     }
                 } else {
-                    stack.push(list.get(j));
+                    stack.add(ch);
                 }
             }
-
+            
             if (stack.isEmpty()) {
                 answer++;
             }
-            deque.addLast(deque.removeFirst());
         }
         return answer;
     }
