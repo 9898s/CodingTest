171c3af18d760286956e815a99932081447172ec
CJ <sueh9898@icloud.com>
2023-06-17 02:45:46 +0900

    [Bronze I] Title: 이항 계수 1, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        System.out.println(combination(N, K));
+    }
+
+    public static int combination(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+        return combination(n - 1, k - 1) + combination(n - 1, k);
+    }
+}
