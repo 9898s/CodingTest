b4b42120f035bdc71be8c47ae2f2a1f11c544a5b
CJ <sueh9898@icloud.com>
2023-06-16 17:30:56 +0900

    [level 2] Title: 기능개발, Time: 2.78 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,12 +1,12 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         Queue<Integer> queue = new LinkedList<>();
         ArrayList<Integer> list = new ArrayList<>();
-
+        
         for (int i = 0; i < progresses.length; i++) {
             if ((100 - progresses[i]) % speeds[i] == 0) {
                 queue.add((100 - progresses[i]) / speeds[i]);
@@ -14,20 +14,23 @@ class Solution {
                 queue.add((100 - progresses[i]) / speeds[i] + 1);
             }
         }
+        
+        int cnt = 1;
+        int num = queue.poll();
 
-        System.out.println(queue);
-
-        while (!queue.isEmpty()) {
-            int day = queue.remove();
-            int count = 1;
-
-            while (!queue.isEmpty() && day >= queue.peek()) {
-                queue.remove();
-                count++;
+        while (queue.size() > 0) {
+            if (num >= queue.peek()) {
+                queue.poll();
+                cnt++;
+            } else {
+                list.add(cnt);
+                num = queue.poll();
+                cnt = 1;
             }
-            list.add(count);
         }
-        int[] answer = list.stream().mapToInt(i -> i).toArray();
+        list.add(cnt);
+        
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
