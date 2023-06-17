35da3b82981f49965de500667871bd69f2514300
CJ <sueh9898@icloud.com>
2023-06-17 15:35:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.22 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,39 +2,37 @@ import java.util.Stack;
 
 class Solution {
     public int solution(String s) {
-        /*
-        1. 문자열 길이 만큼 Deque를 이용해서 왼쪽으로 회전 시켜준다.
-        2. 회전 시킬때마다 Stack을 이용해서 올바른 괄호 문자열인지 확인해준다.
-        */
-        
         int answer = 0;
         
         StringBuilder sb = new StringBuilder(s);
-        for (int i = 0; i < s.length(); i++) {
-            sb.append(sb.charAt(0));
-            sb.delete(0, 1);
-            
-            Stack<Character> stack = new Stack<>();
-            for (char ch : sb.toString().toCharArray()) {
-                if (!stack.isEmpty()) {
-                    if (ch == ')' && stack.peek() == '(') {
-                        stack.pop();
-                    } else if (ch == ']' && stack.peek() == '[') {
-                        stack.pop();
-                    } else if (ch == '}' && stack.peek() == '{') {
-                        stack.pop();
-                    } else {
-                        stack.push(ch);
-                    }
-                } else {
-                    stack.push(ch);
-                }
-            }
-            
-            if (stack.isEmpty()) {
+        for (int i = 0; i < sb.length(); i++) {
+            if (checkMatching(sb.toString())) {
                 answer++;
             }
+            
+            sb.append(sb.charAt(0));
+            sb.delete(0, 1);
         }
         return answer;
+    }
+    
+    public boolean checkMatching(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == ']' || ch == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    char open_ch = stack.pop();
+                    if (open_ch == '(' && ch != ')' || open_ch == '[' && ch != ']' || open_ch == '{' && ch != '}') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
