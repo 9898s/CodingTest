d8d115b3d4c87b721f9e9df0b00db8ae6bd0fa55
CJ <sueh9898@icloud.com>
2023-06-19 22:24:10 +0900

    [Silver III] Title: 1, Time: 204 ms, Memory: 17344 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         while (sc.hasNextInt()) {
             int N = sc.nextInt();
-            int num = 0;
 
-            for (int i = 1; ; i++) {
+            int num = 0;
+            int cnt = 0;
+            while (true) {
                 num = num * 10 + 1;
                 num %= N;
+                cnt++;
 
                 if (num == 0) {
-                    System.out.println(i);
+                    System.out.println(cnt);
                     break;
                 }
             }
