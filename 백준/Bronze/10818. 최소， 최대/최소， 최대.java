e859af0b61dc28c476c93936a116765cb4a1388a
CJ <sueh9898@icloud.com>
2023-07-07 03:12:03 +0900

    [Bronze III] Title: 최소, 최대, Time: 1340 ms, Memory: 106156 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,22 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
-		
-		int[] arr = new int[size];
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		int max = arr[0], min = arr[0];
-		for(int i = 0; i < arr.length; i++) {
-			max = arr[i] > max ? arr[i] : max;
-			min = arr[i] < min ? arr[i] : min;
-		}
-		System.out.printf("%d %d", min, max);
-	}
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] + " " + arr[arr.length - 1]);
+    }
 }
