578fbe0be5127586b67de136b9d555344a58c534
CJ <sueh9898@icloud.com>
2023-07-07 04:24:54 +0900

    [Bronze III] Title: 직각삼각형, Time: 120 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,26 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int[] arr = new int[3];
         while (true) {
-            arr[0] = sc.nextInt();
-            arr[1] = sc.nextInt();
-            arr[2] = sc.nextInt();
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str, " ");
+
+            int[] arr = new int[3];
+            arr[0] = Integer.parseInt(st.nextToken());
+            arr[1] = Integer.parseInt(st.nextToken());
+            arr[2] = Integer.parseInt(st.nextToken());
 
             if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
                 break;
             }
 
             Arrays.sort(arr);
-            if (Math.sqrt(Math.pow(arr[0], 2) + Math.pow(arr[1], 2)) == Math.sqrt(Math.pow(arr[2], 2))) {
-                System.out.println("right");
+
+            if (Math.pow(arr[0], 2) + Math.pow(arr[1], 2) == Math.pow(arr[2], 2)) {
+                sb.append("right").append('\n');
             } else {
-                System.out.println("wrong");
+                sb.append("wrong").append('\n');
             }
         }
+        System.out.print(sb);
     }
 }
