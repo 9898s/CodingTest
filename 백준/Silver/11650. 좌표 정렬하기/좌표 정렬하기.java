3737ded1445f5e4dca223fc66d1634fa4990a643
CJ <sueh9898@icloud.com>
2023-07-07 17:59:13 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 772 ms, Memory: 55880 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,31 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = sc.nextInt();
-        int[][] arr = new int[T][2];
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
-            arr[i][0] = sc.nextInt();
-            arr[i][1] = sc.nextInt();
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str, " ");
+
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 if (o1[0] == o2[0]) {
-                    return Integer.compare(o1[1], o2[1]);
+                    return o1[1] - o2[1];
                 }
-                return Integer.compare(o1[0], o2[0]);
+                return o1[0] - o2[0];
             }
         });
-        
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i][0] + " " + arr[i][1]);
+            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
         }
+        System.out.print(sb);
     }
 }
