27c13a5a73b638e0e89804f11622848fab49ede9
CJ <sueh9898@icloud.com>
2023-07-07 18:34:59 +0900

    [Silver V] Title: 나이순 정렬, Time: 820 ms, Memory: 56120 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,30 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
 
-        String[][] matrix = new String[N][2];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String[][] strings = new String[N][2];
+
         for (int i = 0; i < N; i++) {
-            matrix[i][0] = sc.next();
-            matrix[i][1] = sc.next();
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str, " ");
+
+            strings[i][0] = st.nextToken();
+            strings[i][1] = st.nextToken();
         }
 
-        Arrays.sort(matrix, new Comparator<String[]>() {
+        Arrays.sort(strings, new Comparator<String[]>() {
             @Override
             public int compare(String[] o1, String[] o2) {
-                if (o1[1].equals(o2)) {
-                    return 0;
-                }
                 return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
             }
         });
 
-        for (int i = 0; i < N; i++) {
-            System.out.println(matrix[i][0] + " " + matrix[i][1]);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strings.length; i++) {
+            sb.append(strings[i][0] + " " + strings[i][1]).append('\n');
         }
+        System.out.print(sb);
     }
 }
