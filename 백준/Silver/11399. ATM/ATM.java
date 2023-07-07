865116c7cfbef6952d46ed2fd77863aa3e4280f8
CJ <sueh9898@icloud.com>
2023-07-08 03:51:16 +0900

    [Silver IV] Title: ATM, Time: 136 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,27 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
 
-        int N = sc.nextInt();
         int[] arr = new int[N];
-
         for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
 
-        long sum = 0;
-        for (int i = 0; i < N; i++) {
-            long sum2 = 0;
-            for (int j = 0; j <= i; j++) {
-                sum2 += arr[j];
-            }
-            sum += sum2;
+        int answer = 0;
+        int[] time = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            time[i] = time[i - 1] + arr[i - 1];
+            answer += time[i];
         }
-        System.out.println(sum);
+        System.out.println(answer);
     }
 }
