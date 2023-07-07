eaa724c8efd5b11674b434f3e922ab48e69dd730
CJ <sueh9898@icloud.com>
2023-07-07 18:21:36 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,33 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < N; i++) {
-            String str = sc.next();
-
-            boolean isFlag = false;
-            Stack<Character> stack = new Stack<>();
-
-            for (char ch : str.toCharArray()) {
-                if (ch == '(') {
-                    stack.push(ch);
-                } else {
-                    if (stack.isEmpty()) {
-                        isFlag = true;
-                    } else {
-                        stack.pop();
-                    }
-                }
-            }
-
-            if (!stack.isEmpty()) {
-                isFlag = true;
-            }
-            System.out.println(isFlag ? "NO" : "YES");
+            String str = br.readLine();
+            sb.append(check(str) ? "YES" : "NO").append('\n');
         }
+        System.out.print(sb);
+    }
+
+    public static boolean check(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int j = 0; j < str.length(); j++) {
+            if (str.charAt(j) == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                
+                stack.pop();
+            } else {
+                stack.push(str.charAt(j));
+            }
+        }
+        return stack.isEmpty();
     }
 }
