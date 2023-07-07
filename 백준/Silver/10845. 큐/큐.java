d991a348f9e7599f7fe5cc92b885773de08195b1
CJ <sueh9898@icloud.com>
2023-07-07 17:43:46 +0900

    [Silver IV] Title: 큐, Time: 196 ms, Memory: 19704 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,40 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
         Queue<Integer> queue = new LinkedList<>();
-
         StringBuilder sb = new StringBuilder();
         int last = 0;
 
         for (int i = 0; i < N; i++) {
-            switch (sc.next()) {
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str, " ");
+
+            switch (st.nextToken()) {
                 case "push":
-                    last = sc.nextInt();
-                    queue.add(last);
+                    last = Integer.parseInt(st.nextToken());
+                    queue.offer(last);
                     break;
                 case "pop":
-                    sb.append(queue.isEmpty() ? -1 : queue.poll()).append("\n");
+                    sb.append(queue.isEmpty() ? "-1" : queue.poll()).append('\n');
                     break;
                 case "size":
-                    sb.append(queue.size()).append("\n");
+                    sb.append(queue.size()).append('\n');
                     break;
                 case "empty":
-                    sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+                    sb.append(queue.isEmpty() ? "1" : "0").append('\n');
                     break;
                 case "front":
-                    sb.append(queue.isEmpty() ? -1 : queue.peek()).append("\n");
+                    sb.append(queue.isEmpty() ? "-1" : queue.peek()).append('\n');
                     break;
                 case "back":
-                    sb.append(queue.isEmpty() ? -1 : last).append("\n");
+                    sb.append(queue.isEmpty() ? "-1" : last).append('\n');
                     break;
             }
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
