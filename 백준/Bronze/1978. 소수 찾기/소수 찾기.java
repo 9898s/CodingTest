11bfa52eb58c85917e460dbce0c85ef6999f2691
CJ <sueh9898@icloud.com>
2023-07-07 18:07:01 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,9 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         boolean[] check = new boolean[1001];
         check[0] = check[1] = true;
+
         for (int i = 2; i * i <= 1000; i++) {
             if (check[i]) {
                 continue;
@@ -13,13 +18,16 @@ public class Main {
             }
         }
 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
 
         int answer = 0;
         for (int i = 0; i < N; i++) {
-            int M = sc.nextInt();
-            if (!check[M]) {
+            if (!check[Integer.parseInt(st.nextToken())]) {
                 answer++;
             }
         }
