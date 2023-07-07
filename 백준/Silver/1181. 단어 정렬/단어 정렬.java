6f0fe32e3e43a7105c5bff9048cde3e583a071bf
CJ <sueh9898@icloud.com>
2023-07-07 18:48:20 +0900

    [Silver V] Title: 단어 정렬, Time: 352 ms, Memory: 26168 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,16 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
         String[] strings = new String[N];
 
         for (int i = 0; i < N; i++) {
-            strings[i] = sc.next();
+            strings[i] = br.readLine();
         }
 
         Arrays.sort(strings, new Comparator<String>() {
@@ -18,18 +21,21 @@ public class Main {
             public int compare(String o1, String o2) {
                 if (o1.length() == o2.length()) {
                     return o1.compareTo(o2);
-                } else {
-                    return o1.length() - o2.length();
                 }
+
+                return o1.length() - o2.length();
             }
         });
 
-        System.out.println(strings[0]);
+        StringBuilder sb = new StringBuilder();
+        sb.append(strings[0]).append('\n');
+
         for (int i = 1; i < strings.length; i++) {
             if (strings[i].equals(strings[i - 1])) {
                 continue;
             }
-            System.out.println(strings[i]);
+            sb.append(strings[i]).append('\n');
         }
+        System.out.print(sb);
     }
 }
